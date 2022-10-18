b0c7d3c4388a1128bd3ea4f40797482726ae9b6e
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-10-18 15:07:49 +0530

    function

---
+++ function.js
@@ -0,0 +1,12 @@
+function add(a,b){
+    return a+b;
+}
+// fun1= function add(a,b){
+//     return a+b;
+// }
+
+fun1= function (a,b){ // anonymous funtion 
+    return a+b;
+}
+console.log(fun1(10,13));
+console.log(3==3.0);
